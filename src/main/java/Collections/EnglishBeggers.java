5d339f13601c4e7ffb5efc170ef4be4cbd6fee5b
DamianTujdowski <d.tujdowski@gmail.com>
2019-01-30 21:27:32 +0100

    Final version

--- src/main/java/Collections/EnglishBeggers.java
+++ src/main/java/Collections/EnglishBeggers.java
@@ -1,4 +1,14 @@
 package Collections;
 
+
 public class EnglishBeggers {
+    public int[] almsCount(int[] values, int n) {
+        int[] result = new int[n];
+        for (int i = 0; i < result.length; i++) {
+            for (int j = i; j < values.length; j += n) {
+                result[i] += values[j];
+            }
+        }
+        return result;
+    }
 }
