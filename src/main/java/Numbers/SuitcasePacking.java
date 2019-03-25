ae15dbb0d46e6d45838ab2437c5036aa0a07f0da
DamianTujdowski <d.tujdowski@gmail.com>
2019-03-25 23:06:03 +0100

    Packing a suitcase

--- src/main/java/Numbers/SuitcasePacking.java
+++ src/main/java/Numbers/SuitcasePacking.java
@@ -1,4 +1,9 @@
 package Numbers;
 
 public class SuitcasePacking {
+    public boolean fitIn(int a, int b, int m, int n) {
+        return a * a + b * b <= m * n
+                && m >= a && m >= b
+                && n >= a && n >= b;
+    }
 }
