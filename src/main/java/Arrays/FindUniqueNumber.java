bd1453557b007eef97556f4ec2faac86f7e84185
DamianTujdowski <d.tujdowski@gmail.com>
2019-03-25 23:05:08 +0100

    Finding unique number in an arrya

--- src/main/java/Arrays/FindUniqueNumber.java
+++ src/main/java/Arrays/FindUniqueNumber.java
@@ -1,4 +1,23 @@
 package Arrays;
 
+import java.util.Arrays;
+import java.util.Map;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
 public class FindUniqueNumber {
+
+    public static Double findDistinctNumberInArray(double[] arr) {
+        double result = 0;
+        Map<Double, Long> map = Arrays.stream(arr)
+                .boxed()
+                .collect(groupingBy(Double::doubleValue, counting()));
+
+        for (Map.Entry<Double, Long> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                result = entry.getKey();
+            }
+        }
+        return result;
+    }
 }
