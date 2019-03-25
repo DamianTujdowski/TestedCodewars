db9f3d86dde40a81d0c72527b6352bf221da1f3b
DamianTujdowski <d.tujdowski@gmail.com>
2019-03-25 23:07:56 +0100

    Basic encryption algorithm

--- src/main/java/Strings/BasicEncryption.java
+++ src/main/java/Strings/BasicEncryption.java
@@ -1,4 +1,14 @@
 package Strings;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class BasicEncryption {
+
+    public String encrypt(String text, int value) {
+        return IntStream.range(0, text.length())
+                .map(x -> text.charAt(x) + value)
+                .mapToObj(x -> String.valueOf((char)(x % 256)))
+                .collect(Collectors.joining());
+    }
 }
