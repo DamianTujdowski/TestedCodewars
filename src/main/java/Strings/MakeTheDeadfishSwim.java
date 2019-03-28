c04c60b434968316dc98c82ac8a988e524c3751b
DamianTujdowski <d.tujdowski@gmail.com>
2019-03-28 22:45:06 +0100

    Deadfish swim cata

--- src/main/java/Strings/MakeTheDeadfishSwim.java
+++ src/main/java/Strings/MakeTheDeadfishSwim.java
@@ -1,4 +1,31 @@
 package Strings;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MakeTheDeadfishSwim {
+    public int[] parser(String data) {
+        String[] input = data.split("[^idso]*");
+        int temp = 0;
+        List<Integer> result = new ArrayList<>();
+
+        for (String letter : input) {
+            switch (letter) {
+                case "i":
+                    temp++;
+                    break;
+                case "d":
+                    temp--;
+                    break;
+                case "s":
+                    temp *= temp;
+                    break;
+                case "o":
+                    result.add(temp);
+                    break;
+            }
+        }
+
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
 }
