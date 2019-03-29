9c2509d81406de0f1b2dc098d73fc12023933661
DamianTujdowski <d.tujdowski@gmail.com>
2019-03-29 22:23:59 +0100

    String mexican wave

---
+++ src/main/java/Arrays/MexicanWave.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MexicanWave {
+    public String[] waveCreator(String str) {
+        String[] wordsArray = new String[str.length()];
+        Arrays.fill(wordsArray, str);
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < wordsArray.length; i++) {
+            String[] lettersArray = wordsArray[i].split("");
+            lettersArray[i] = lettersArray[i].toUpperCase();
+            wordsArray[i] = String.join("", lettersArray);
+            if (!lettersArray[i].equals(" ")) {
+                result.add(wordsArray[i]);
+            }
+        }
+        return result.stream().toArray(String[]::new);
+    }
+}
